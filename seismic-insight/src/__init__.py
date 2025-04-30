eb900225cfa81dd4c5780fc5952e2de32ebf31e2
DGZH01 <dyl.coronel@gmail.com>
2025-04-30 10:19:50 +0000

    update

--- seismic-insight/src/__init__.py
+++ seismic-insight/src/__init__.py
@@ -4,4 +4,3 @@ from .data_cleaning import clean_earthquake_data
 from .analysis import perform_analysis
 from .visualization import plot_magnitude_over_time, map_epicenters
 from .database_utils import create_earthquake_table, insert_earthquake_data, query_most_powerful_earthquake
-from .llm_helper import generate_summary_from_data
